bb1596076d5ed4f4dc9673e9fa6b0b953c3303f5
Wiktor Mociun <voter101@gmail.com>
2014-11-10 22:12:55 +0100

    Dashboard should encourage user to create city if he/she hasn't got one.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class DashboardController < ApplicationController
+
+  respond_to :html
+
   def index
+    render "index", locals: { has_city: has_current_user_city? }
+  end
+
+  private
+  def has_current_user_city?
+    City.where(owner_id: current_user.id).count != 0
   end
 end
